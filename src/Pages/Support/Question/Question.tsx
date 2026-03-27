57ed64e65de48b09c2070ca46fa2f77529a5ad8d
ArhipSizov <arhip.sizov111@gmail.com>
2026-03-27 21:03:33 +0300

    all adaptive (320x420px)

--- src/Pages/Support/Question/Question.tsx
+++ src/Pages/Support/Question/Question.tsx
@@ -8,16 +8,19 @@ interface type {
   name: string;
   description: string;
 }
-export default function Question({
-  name,
-  description
-}: type) {
+export default function Question({ name, description }: type) {
   const [descriptionShow, setDescriptionShow] = useState<boolean>(false);
   console.log(name, description);
-  
+
   return (
-    <div onClick={() => setDescriptionShow(!descriptionShow)} className="question">
+    <div
+      onClick={() => setDescriptionShow(!descriptionShow)}
+      className="question"
+    >
       <p className="name">{T(name)}</p>
+      {(descriptionShow && <img className="img_1" src="/backAlt.svg" alt="" />) || (
+        <img className="img_2"  src="/backAlt.svg" alt="" />
+      )}
       {descriptionShow && <p className="description">{T(description)}</p>}
     </div>
   );
